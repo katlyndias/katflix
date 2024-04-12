6441722dc3ef145a180949f3a8bcc23f8d863c98
katlyndias <148639020+katlyndias@users.noreply.github.com>
2024-04-11 22:13:51 -0300

    Initial commit

---
+++ src/main/java/br/ada/katflix/KatflixApplication.java
@@ -0,0 +1,13 @@
+package br.ada.katflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ada/katflix/KatflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ada.katflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
